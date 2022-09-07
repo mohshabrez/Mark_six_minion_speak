402bec05756a84991b6b63051b18036bf904032a
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2022-09-07 17:34:12 +0530

    test-minonspeak

---
+++ app.js
@@ -0,0 +1,17 @@
+var btnTranslate = document.querySelector("#btn-translator");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+// outputDiv.innerText = "Shabrez"
+
+function getTranslationURL
+
+function clickHandler(){
+   outputDiv.innerText = "wdheduwvwjfryf" + txtInput.value;
+
+};
+
+btnTranslate.addEventListener("click", clickHandler)
+
+
+
+// eloquentjavascript.net

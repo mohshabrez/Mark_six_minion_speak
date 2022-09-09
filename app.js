350a3a1e666d3ceafc7e1beeb95d9cea6c4c91ec
Shabrez G <31725739+mohshabrez@users.noreply.github.com>
2022-09-09 14:01:30 +0530

    Test-Minion speak

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-var btnTranslate = document.querySelector("#btn-translator");
+var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 // outputDiv.innerText = "Shabrez"
 
-function getTranslationURL
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input){
+   return serverURL + "?" + "text=" + input
+
+}
+
+function errorHandler(error){
+   console.log("error occured", error);
+   alert("something wrong with server: try again after some time");
+}
 
 function clickHandler(){
-   outputDiv.innerText = "wdheduwvwjfryf" + txtInput.value;
+   // outputDiv.innerText = "wdheduwvwjfryf" + txtInput.value;
+   var inputText = txtInput.value;
 
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;})
+
+      // console.log(json.contents.translated))
+   .catch(errorHandler)
 };
 
 btnTranslate.addEventListener("click", clickHandler)
